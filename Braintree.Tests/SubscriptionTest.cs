5183dfd1c71e980d90ca921a704af3b4da7722e4
Braintree <code@getbraintree.com>
2010-03-09 15:07:13 -0600

    Updated a test to support cross-platform time zone ids.

--- Braintree.Tests/SubscriptionTest.cs
+++ Braintree.Tests/SubscriptionTest.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Text.RegularExpressions;
 using NUnit.Framework;
@@ -55,7 +55,7 @@ namespace Braintree.Tests
             Subscription subscription = result.Target;
 
             DateTime now = DateTime.Now;
-            DateTime mountainDate = TimeZoneInfo.ConvertTime(now, TimeZoneInfo.Local, TimeZoneInfo.FindSystemTimeZoneById("Mountain Standard Time"));
+            DateTime mountainDate = TimeZoneInfo.ConvertTime(now, TimeZoneInfo.Local, TimeZoneInfo.FindSystemTimeZoneById("America/Los_Angeles"));
 
             DateTime expectedBillingPeriodEndDate = mountainDate.AddMonths(plan.BillingFrequency).AddDays(-1);
 
@@ -93,7 +93,7 @@ namespace Braintree.Tests
             Subscription subscription = result.Target;
 
             DateTime now = DateTime.Now;
-            DateTime mountainDate = TimeZoneInfo.ConvertTime(now, TimeZoneInfo.Local, TimeZoneInfo.FindSystemTimeZoneById("Mountain Standard Time"));
+            DateTime mountainDate = TimeZoneInfo.ConvertTime(now, TimeZoneInfo.Local, TimeZoneInfo.FindSystemTimeZoneById("America/Los_Angeles"));
 
             DateTime expectedFirstAndNextDate = mountainDate.AddDays(plan.TrialDuration);
 
