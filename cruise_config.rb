12de0d93d20f2ea35e26fd81c8c12415a1cf8416
Braintree <code@getbraintree.com>
2010-03-15 11:42:56 -0500

    Fixed cruise_config.rb

--- cruise_config.rb
+++ cruise_config.rb
@@ -2,7 +2,7 @@ Project.configure do |project|
   project.scheduler.polling_interval = 1.minute
 
   case project.name
-  when "client_library_dotnet_integration_master"
+  when "client_library_java_integration_master"
     project.build_command = "CRUISE_BUILD=#{project.name} GATEWAY_URL='http://localhost:6010' SPHINX_PORT=6322 rake cruise"
     project.triggered_by :gateway_master
   end
