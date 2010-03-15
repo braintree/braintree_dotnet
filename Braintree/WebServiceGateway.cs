34268681d24574c7d06525382b16c807c8cbb47a
Braintree <code@getbraintree.com>
2010-03-15 13:44:01 -0500

    Added accept header.

--- Braintree/WebServiceGateway.cs
+++ Braintree/WebServiceGateway.cs
@@ -48,6 +48,7 @@ namespace Braintree
                 var request = WebRequest.Create(Configuration.BaseMerchantURL() + URL) as HttpWebRequest;
                 request.Headers.Add("Authorization", Configuration.GetAuthorizationHeader());
                 request.Headers.Add("X-ApiVersion", "1");
+                request.Accept = "application/xml";
                 request.UserAgent = "Braintree .NET " + typeof(WebServiceGateway).Assembly.GetName().Version.ToString();
                 request.Method = method;
                 request.KeepAlive = false;
