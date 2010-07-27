166b28fbae43f373938bc5f4148a12b75e5cf6ac
pg and phinze <code@getbraintree.com>
2010-07-27 16:02:56 -0500

    [#1920] - Extract common modification logic into a base ModificationRequest.

--- Braintree/AddModificationRequest.cs
+++ Braintree/AddModificationRequest.cs
@@ -1,13 +1,10 @@
 using System;
+
 namespace Braintree
 {
-    public class AddModificationRequest : Request
+    public class AddModificationRequest : ModificationRequest
     {
-        public Decimal? Amount { get; set; }
         public String InheritedFromId { get; set; }
-        public Boolean? NeverExpires { get; set; }
-        public Int32? NumberOfBillingCycles { get; set; }
-        public Int32? Quantity { get; set; }
 
         public override String ToXml(String root)
         {
@@ -16,13 +13,8 @@ namespace Braintree
 
         protected virtual RequestBuilder BuildRequest(String root)
         {
-            return new RequestBuilder(root).
-                AddElement("amount", Amount).
-                AddElement("inherited-from-id", InheritedFromId).
-                AddElement("never-expires", NeverExpires).
-                AddElement("number-of-billing-cycles", NumberOfBillingCycles).
-                AddElement("quantity", Quantity);
+            return base.BuildRequest(root).
+                AddElement("inherited-from-id", InheritedFromId);
         }
     }
 }
-
--- Braintree/UpdateModificationRequest.cs
+++ Braintree/UpdateModificationRequest.cs
@@ -1,13 +1,10 @@
 using System;
+
 namespace Braintree
 {
-    public class UpdateModificationRequest : Request
+    public class UpdateModificationRequest : ModificationRequest
     {
-        public Decimal? Amount { get; set; }
         public String ExistingId { get; set; }
-        public Boolean? NeverExpires { get; set; }
-        public Int32? NumberOfBillingCycles { get; set; }
-        public Int32? Quantity { get; set; }
 
         public override String ToXml(String root)
         {
@@ -16,13 +13,8 @@ namespace Braintree
 
         protected virtual RequestBuilder BuildRequest(String root)
         {
-            return new RequestBuilder(root).
-                AddElement("amount", Amount).
-                AddElement("existing-id", ExistingId).
-                AddElement("never-expires", NeverExpires).
-                AddElement("number-of-billing-cycles", NumberOfBillingCycles).
-                AddElement("quantity", Quantity);
+            return base.BuildRequest(root).
+                AddElement("existing-id", ExistingId);
         }
     }
 }
-
