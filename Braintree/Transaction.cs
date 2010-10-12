1c24bfb1bf754c91a062d4d4cddc80940d136b70
drew and hammer <code@getbraintree.com>
2010-10-12 17:45:30 -0500

    [#2097] - Removed SETTLEMENT_FAILED status, and added SETTLING status.

--- Braintree/Transaction.cs
+++ Braintree/Transaction.cs
@@ -29,14 +29,14 @@ namespace Braintree
         public static readonly TransactionStatus GATEWAY_REJECTED = new TransactionStatus("gateway_rejected");
         public static readonly TransactionStatus PROCESSOR_DECLINED = new TransactionStatus("processor_declined");
         public static readonly TransactionStatus SETTLED = new TransactionStatus("settled");
-        public static readonly TransactionStatus SETTLEMENT_FAILED = new TransactionStatus("settlement_failed");
+        public static readonly TransactionStatus SETTLING = new TransactionStatus("settling");
         public static readonly TransactionStatus SUBMITTED_FOR_SETTLEMENT = new TransactionStatus("submitted_for_settlement");
         public static readonly TransactionStatus UNRECOGNIZED = new TransactionStatus("unrecognized");
         public static readonly TransactionStatus VOIDED = new TransactionStatus("voided");
 
         public static readonly TransactionStatus[] ALL = {
             AUTHORIZED, AUTHORIZING, FAILED, GATEWAY_REJECTED, PROCESSOR_DECLINED, SETTLED,
-            SETTLEMENT_FAILED, SUBMITTED_FOR_SETTLEMENT, VOIDED
+            SETTLING, SUBMITTED_FOR_SETTLEMENT, VOIDED
         };
 
         protected TransactionStatus(String name) : base(name) {}
