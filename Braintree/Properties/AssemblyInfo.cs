d22c97db93d143975df535b0106f7a2df99c8451
Braintree <code@getbraintree.com>
2012-04-19 17:17:21 -0500

    2.15.1

--- Braintree/Properties/AssemblyInfo.cs
+++ Braintree/Properties/AssemblyInfo.cs
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("2.15.0.0")]
-[assembly: AssemblyFileVersion("2.15.0.0")]
+[assembly: AssemblyVersion("2.15.1.0")]
+[assembly: AssemblyFileVersion("2.15.1.0")]
