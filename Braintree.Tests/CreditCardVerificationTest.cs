5046bb466187d46fa01db95fe373a51bcab0f7bd
cosgrove and mnussbaum <code@getbraintree.com>
2012-10-23 19:32:34 +0000

    cleanup - readds erroneously deleted verification tests

--- Braintree.Tests/CreditCardVerificationTest.cs
+++ Braintree.Tests/CreditCardVerificationTest.cs
@@ -25,6 +25,63 @@ namespace Braintree.Tests
         }
 
         [Test]
+        public void ConstructFromResponse()
+        {
+            StringBuilder builder = new StringBuilder();
+            builder.Append("<?xml version=\"1.0\" encoding=\"UTF-8\"?>");
+            builder.Append("<api-error-response>");
+            builder.Append("  <verification>");
+            builder.Append("    <avs-error-response-code nil=\"true\"></avs-error-response-code>");
+            builder.Append("    <avs-postal-code-response-code>I</avs-postal-code-response-code>");
+            builder.Append("    <status>processor_declined</status>");
+            builder.Append("    <processor-response-code>2000</processor-response-code>");
+            builder.Append("    <avs-street-address-response-code>I</avs-street-address-response-code>");
+            builder.Append("    <processor-response-text>Do Not Honor</processor-response-text>");
+            builder.Append("    <cvv-response-code>M</cvv-response-code>");
+            builder.Append("  </verification>");
+            builder.Append("  <errors>");
+            builder.Append("    <errors type=\"array\"/>");
+            builder.Append("  </errors>");
+            builder.Append("</api-error-response>");
+
+            XmlDocument doc = new XmlDocument();
+            doc.LoadXml(builder.ToString());
+
+            CreditCardVerification verification = new CreditCardVerification(new NodeWrapper(doc).GetNode("//verification"));
+            Assert.AreEqual(null, verification.AvsErrorResponseCode);
+            Assert.AreEqual("I", verification.AvsPostalCodeResponseCode);
+            Assert.AreEqual(VerificationStatus.PROCESSOR_DECLINED, verification.Status);
+            Assert.AreEqual("2000", verification.ProcessorResponseCode);
+            Assert.AreEqual("I", verification.AvsStreetAddressResponseCode);
+            Assert.AreEqual("Do Not Honor", verification.ProcessorResponseText);
+            Assert.AreEqual("M", verification.CvvResponseCode);
+        }
+
+        [Test]
+        public void ConstructFromResponseWithNoVerification()
+        {
+            StringBuilder builder = new StringBuilder();
+            builder.Append("<?xml version=\"1.0\" encoding=\"UTF-8\"?>");
+            builder.Append("<api-error-response>");
+            builder.Append("  <errors>");
+            builder.Append("    <errors type=\"array\"/>");
+            builder.Append("  </errors>");
+            builder.Append("</api-error-response>");
+
+            XmlDocument doc = new XmlDocument();
+            doc.LoadXml(builder.ToString());
+
+            CreditCardVerification verification = new CreditCardVerification(new NodeWrapper(doc).GetNode("//verification"));
+            Assert.AreEqual(null, verification.AvsErrorResponseCode);
+            Assert.AreEqual(null, verification.AvsPostalCodeResponseCode);
+            Assert.AreEqual(null, verification.Status);
+            Assert.AreEqual(null, verification.ProcessorResponseCode);
+            Assert.AreEqual(null, verification.AvsStreetAddressResponseCode);
+            Assert.AreEqual(null, verification.ProcessorResponseText);
+            Assert.AreEqual(null, verification.CvvResponseCode);
+        }
+
+        [Test]
         public void Search_OnMultipleValueFields()
         {
             var createRequest = new CustomerRequest
