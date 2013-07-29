4ed77fa2004ed9bc73de6377cc774ea2f3553928
adam and katelyn <code@getbraintree.com>
2013-07-29 14:14:37 -0500

    Bumped version and updated changelog

--- Braintree/Properties/AssemblyInfo.cs
+++ Braintree/Properties/AssemblyInfo.cs
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("2.23.1.0")]
-[assembly: AssemblyFileVersion("2.23.1.0")]
+[assembly: AssemblyVersion("2.24.1.0")]
+[assembly: AssemblyFileVersion("2.24.1.0")]
