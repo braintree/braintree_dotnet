504e9b9b6c2b9481fa84ac9cdf6de1646e73bb9e
Braintree <code@getbraintree.com>
2013-08-14 18:59:53 +0000

    2.25.2

--- Braintree/WebhookNotification.cs
+++ Braintree/WebhookNotification.cs
@@ -6,6 +6,8 @@ namespace Braintree
 {
     public class WebhookKind : Enumeration
     {
+        public static readonly WebhookKind PARTNER_USER_CREATED = new WebhookKind("partner_user_created");
+        public static readonly WebhookKind PARTNER_USER_DELETED = new WebhookKind("partner_user_deleted");
         public static readonly WebhookKind SUBSCRIPTION_CANCELED = new WebhookKind("subscription_canceled");
         public static readonly WebhookKind SUBSCRIPTION_CHARGED_SUCCESSFULLY = new WebhookKind("subscription_charged_successfully");
         public static readonly WebhookKind SUBSCRIPTION_CHARGED_UNSUCCESSFULLY = new WebhookKind("subscription_charged_unsuccessfully");
@@ -19,6 +21,8 @@ namespace Braintree
         public static readonly WebhookKind TRANSACTION_DISBURSED = new WebhookKind("transaction_disbursed");
 
         public static readonly WebhookKind[] ALL = {
+          PARTNER_USER_CREATED,
+          PARTNER_USER_DELETED,
           SUBSCRIPTION_CANCELED,
           SUBSCRIPTION_CHARGED_SUCCESSFULLY,
           SUBSCRIPTION_CHARGED_UNSUCCESSFULLY,
@@ -43,6 +47,7 @@ namespace Braintree
         public String Message { get; protected set; }
         public DateTime? Timestamp { get; protected set; }
         public Transaction Transaction { get; protected set; }
+        public PartnerCredentials PartnerCredentials { get; protected set; }
 
         public WebhookNotification(NodeWrapper node, BraintreeService service)
         {
@@ -67,13 +72,17 @@ namespace Braintree
               Transaction = new Transaction(WrapperNode.GetNode("transaction"), service);
             }
 
+            if (WrapperNode.GetNode("partner-credentials") != null) {
+              PartnerCredentials = new PartnerCredentials(WrapperNode.GetNode("partner-credentials"));
+            }
+
             if (WrapperNode.GetNode("errors") != null) {
               Errors = new ValidationErrors(WrapperNode.GetNode("errors"));
             }
+
             if (WrapperNode.GetNode("message") != null) {
               Message = WrapperNode.GetString("message");
             }
-
         }
     }
 }
--- Braintree/WebhookTestingGateway.cs
+++ Braintree/WebhookTestingGateway.cs
@@ -43,29 +43,115 @@ namespace Braintree
                 return MerchantAccountDeclinedSampleXml(id);
             } else if (kind == WebhookKind.TRANSACTION_DISBURSED) {
                 return TransactionDisbursedSampleXml(id);
+            } else if (kind == WebhookKind.PARTNER_USER_CREATED) {
+                return PartnerUserCreatedSampleXml(id);
+            } else if (kind == WebhookKind.PARTNER_USER_DELETED) {
+                return PartnerUserDeletedSampleXml(id);
             } else {
                 return SubscriptionXml(id);
             }
         }
 
+        private static readonly string TYPE_DATETIME = "type=\"datetime\"";
+        private static readonly string TYPE_ARRAY = "type=\"array\"";
+        private static readonly string TYPE_SYMBOL = "type=\"symbol\"";
+
         private String MerchantAccountDeclinedSampleXml(String id)
         {
-            return String.Format("<api-error-response><message>Applicant declined due to OFAC.</message><merchant-account><id>{0}</id><master-merchant-account><id>master_ma_for_{0}</id><status>suspended</status></master-merchant-account><status>suspended</status></merchant-account><errors><merchant-account><errors type=\"array\"><error><message>Applicant declined due to OFAC.</message><code>82621</code><attribute>base</attribute></error></errors></merchant-account></errors></api-error-response>", id);
+            return node("api-error-response",
+                node("message", "Applicant declined due to OFAC."),
+                node_attr("errors", TYPE_ARRAY,
+                    node("merchant-account",
+                        node_attr("errors", TYPE_ARRAY,
+                            node("error",
+                                node("code", "82621"),
+                                node("message", "Applicant declined due to OFAC."),
+                                node_attr("attribute", TYPE_SYMBOL, "base")
+                            )
+                        )
+                    )
+                ),
+                node("merchant-account",
+                    node("id", id),
+                    node("status", "suspended"),
+                    node("master-merchant-account",
+                        node("id", "master_ma_for_" + id),
+                        node("status", "suspended")
+                    )
+                )
+            );
         }
 
         private String TransactionDisbursedSampleXml(String id)
         {
-            return String.Format("<transaction><id>{0}</id><amount>100</amount><disbursement-details><disbursement-date type=\"datetime\">2013-07-09T18:23:29Z</disbursement-date></disbursement-details><billing></billing><credit-card></credit-card><customer></customer><descriptor></descriptor><shipping></shipping><subscription></subscription></transaction>", id);
+            return node("transaction",
+                    node("id", id),
+                    node("amount", "100"),
+                    node("disbursement-details",
+                        node_attr("disbursement-date", TYPE_DATETIME, "2013-07-09T18:23:29Z")
+                    ),
+                    node("billing"),
+                    node("credit-card"),
+                    node("customer"),
+                    node("descriptor"),
+                    node("shipping"),
+                    node("subscription")
+            );
         }
 
         private String SubscriptionXml(String id)
         {
-            return String.Format("<subscription><id>{0}</id><transactions type=\"array\"></transactions><add-ons type=\"array\"></add-ons><discounts type=\"array\"></discounts></subscription>", id);
+            return node("subscription",
+                    node("id", id),
+                    node_attr("transactions", TYPE_ARRAY),
+                    node_attr("add_ons", TYPE_ARRAY),
+                    node_attr("discounts", TYPE_ARRAY)
+            );
         }
 
         private String MerchantAccountApprovedSampleXml(String id)
         {
-            return String.Format("<merchant-account><id>{0}</id><master-merchant-account><id>master_ma_for_{0}</id><status>active</status></master-merchant-account><status>active</status></merchant-account>", id);
+            return node("merchant-account",
+                    node("id", id),
+                    node("master-merchant-account",
+                        node("id", "master_ma_for_" + id),
+                        node("status", "active")
+                    ),
+                    node("status", "active")
+            );
+        }
+
+        private String PartnerUserCreatedSampleXml(String id) {
+            return node("partner-credentials",
+                    node("partner-user-id", "abc123"),
+                    node("merchant-public-id", "public_id"),
+                    node("public-key", "public_key"),
+                    node("private-key", "private_key")
+            );
+        }
+
+        private String PartnerUserDeletedSampleXml(String id) {
+            return node("partner-credentials",
+                    node("partner-user-id", "abc123")
+            );
+        }
+
+        private static string node(string name, params string[] contents) {
+            return node_attr(name, null, contents);
+        }
+
+        private static string node_attr(string name, string attributes, params string[] contents) {
+            StringBuilder buffer = new StringBuilder();
+            buffer.Append('<').Append(name);
+            if (attributes != null) {
+                buffer.Append(" ").Append(attributes);
+            }
+            buffer.Append('>');
+            foreach (string content in contents) {
+                buffer.Append(content);
+            }
+            buffer.Append("</").Append(name).Append('>');
+            return buffer.ToString();
         }
     }
 }
