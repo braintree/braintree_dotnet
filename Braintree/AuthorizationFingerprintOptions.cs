870804dbb752166ae2f16d53d0b12f6e0311ebd6
dix and lionel <code@getbraintree.com>
2013-12-11 17:14:23 +0000

    cleanup - Use nullable boolean type for auth fingerprint options.

--- Braintree/AuthorizationFingerprintOptions.cs
+++ Braintree/AuthorizationFingerprintOptions.cs
@@ -6,8 +6,8 @@ namespace Braintree
 {
     public class AuthorizationFingerprintOptions {
         public String CustomerId { get; set; }
-        public Boolean MakeDefault { get; set; }
-        public Boolean FailOnDuplicatePaymentMethod { get; set; }
-        public Boolean VerifyCard { get; set; }
+        public Boolean? MakeDefault { get; set; }
+        public Boolean? FailOnDuplicatePaymentMethod { get; set; }
+        public Boolean? VerifyCard { get; set; }
     }
 }
